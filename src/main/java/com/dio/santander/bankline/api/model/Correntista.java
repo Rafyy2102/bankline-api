2bf63d2469cb356d0b0fa194818da7083b1df00e
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2022-05-06 11:44:27 -0300

    entity mapping

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,10 +1,22 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 20)
 	private String cpf;	
+	
+	@Column(length = 60 )
 	private String nome;
 	
+	@Embedded
 	private Conta conta;
 	
 	public Conta getConta() {
--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -1,13 +1,23 @@
 package com.dio.santander.bankline.api.model;
 
 import java.time.LocalDateTime;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "data_hora")
 	private LocalDateTime dataHora;
-	private String descricao;
+	
+	private String descricao;	
 	private Double valor;
+	
+	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
 	
 	public Integer getId() {
